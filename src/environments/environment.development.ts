d2f3b21d420c025c549f24f4e671165f464346ab
larissajuliavsa <larissa.jvsa@gmail.com>
2024-02-27 15:18:41 -0300

    build: project initial config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,20 @@
+export const environment = {
+    production: true
+  };
+
+
+export const SpotifyConfiguration = {
+  clientId: '8b147af959ac4ece8c77946d18d54e2d',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+    "user-library-read",
+    "playlist-read-private",
+    "playlist-read-collaborative"
+  ]
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false
+};
+
+export const SpotifyConfiguration = {
+  clientId: '8b147af959ac4ece8c77946d18d54e2d',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+    "user-library-read",
+    "playlist-read-private",
+    "playlist-read-collaborative"
+  ]
+}
