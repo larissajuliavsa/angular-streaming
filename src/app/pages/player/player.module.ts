78c63d20dd9839db2801dc7bfbb20e54e17e5aaa
larissajuliavsa <larissa.jvsa@gmail.com>
2024-03-06 19:12:09 -0300

    feat: add player component

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 import { PlayerComponent } from './player.component';
 import { RouterModule } from '@angular/router';
 import { PlayerRoutes } from './player.routes';
+import { SidebarComponent } from 'src/app/components/sidebar/sidebar.component';
+import { MenuButtonComponent } from 'src/app/components/menu-button/menu-button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
 @NgModule({
   declarations: [
-    PlayerComponent
+    PlayerComponent,
+    SidebarComponent,
+    MenuButtonComponent
   ],
   imports: [
     CommonModule,
+    FontAwesomeModule,
     RouterModule.forChild(PlayerRoutes)
   ]
 })
